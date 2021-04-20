1d7101368dbb3690c006f1c6db091468eb102ed3
paolofersa <anthony.fernandez@ucsp.edu.pe>
2021-04-20 17:33:48 -0500

    v2

--- quadTree_implementacion/source/main.cpp
+++ quadTree_implementacion/source/main.cpp
@@ -39,6 +39,7 @@ class Quadtree
 {
 public:
     Quadtree(Marco limite, int n);
+    ~Quadtree();
     bool insertarPto(Punto pto);
 
 private:
@@ -47,12 +48,10 @@ private:
     vector<Punto> Puntos;
     bool dividido;
 
-    Quadtree* derechaArriba = 0;
-    Quadtree* derechaAbajo = 0;
-    Quadtree* izquierdaAbajo = 0;
-    Quadtree* izquierdaArriba = 0;
-    vector<Punto> Puntos;
-
+    Quadtree* derechaArriba;
+    Quadtree* derechaAbajo;
+    Quadtree* izquierdaAbajo;
+    Quadtree* izquierdaArriba;
 
     void dividir();
 };
@@ -62,6 +61,34 @@ Quadtree::Quadtree(Marco limite, int n)
     limiteMarco = limite;
     profundidad = n;
     dividido = false;
+    derechaArriba = derechaAbajo = izquierdaAbajo = izquierdaArriba = 0;
+}
+
+Quadtree::~Quadtree()
+{
+    Quadtree* destructor;
+    if (dividido) {
+        for (destructor = derechaArriba; destructor && destructor->dividido; destructor = destructor->derechaArriba)
+        {
+            delete destructor;
+        }
+        for (destructor = derechaAbajo; destructor && destructor->dividido; destructor = destructor->derechaAbajo)
+        {
+            delete destructor;
+        }
+        for (destructor = izquierdaAbajo; destructor && destructor->dividido; destructor = destructor->izquierdaAbajo)
+        {
+            delete destructor;
+        }
+        for (destructor = izquierdaArriba; destructor && destructor->dividido; destructor = destructor->izquierdaArriba)
+        {
+            delete destructor;
+        }
+        delete derechaArriba;
+        delete derechaAbajo;
+        delete izquierdaAbajo;
+        delete izquierdaArriba;
+    }
 }
 
 bool Quadtree::insertarPto(Punto pto) {
@@ -106,9 +133,16 @@ int main(){
     
     Marco marcoExterior(50,50,50);
     Quadtree QT(marcoExterior, 2);
+
     Punto p1(24,60);
     Punto p2(50, 90);
+    Punto p3(70, 30);
+    Punto p4(20, 20);
 
+    QT.insertarPto(p1);
+    QT.insertarPto(p2);
+    QT.insertarPto(p3);
+    QT.insertarPto(p4);
 
     return 0;
 }
