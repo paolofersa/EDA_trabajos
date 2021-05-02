1b91aba1660ad18874fcee1c1520c5a21485e789
paolofersa <anthony.fernandez@ucsp.edu.pe>
2021-05-01 23:58:34 -0500

    dibujado

--- heat map_quadTree/source/actions.js
+++ heat map_quadTree/source/actions.js
@@ -5,26 +5,21 @@ L.tileLayer(`https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png`, {
 }).addTo(myMap);
 
 myMap.doubleClickZoom.disable()
-
+/*
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ], color='#800026').addTo(myMap);
-
-// define rectangle geographical bounds
-var bounds = [[51.509, -0.08], [51.503, -0.06]];
-var colorRect = "#800026";
-// create a rectangle
-L.rectangle(bounds, {color: colorRect, weight: 1}).addTo(myMap);
-
+*/
 getData();
-var p1x, p1y, p2x, p2y, col;
+var p1x, p1y, p2x, p2y;
+var bounds, colorRect;
 //leyendo el archivo
 async function getData(){
     const response = await fetch('../build/provisional.txt')
     const data = await response.text();
-
+    
     const rows = data.split('\n');
     rows.forEach(i=>{
         const row = i.split('\t');
@@ -32,8 +27,10 @@ async function getData(){
         p1y = row[1];
         p2x = row[2];
         p2y = row[3];
-        col = row[4];
-        console.log(p1x,p1y);
+        colorRect = row[4];
+
+        bounds = [[p1x, p1y], [p2x, p2y]];
+        L.rectangle(bounds, {color: colorRect, weight: 1}).addTo(myMap);
     });
 
     console.log(rows);
